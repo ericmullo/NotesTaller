f132f48570acd1128a813d13bcdef1c0481869b7
ericmullo <ericrodrigomullo@gmail.com>
2024-12-16 21:13:36 -0500

    Nuevas modificaciones, agregue ViewModels y modifique Views y Models

--- Notes/Views/AboutPage.xaml.cs
+++ Notes/Views/AboutPage.xaml.cs
@@ -1,30 +1,9 @@
-using Microsoft.Maui.Controls;
-using Microsoft.Maui.ApplicationModel;
+namespace Notes.Views;
 
-namespace Notes.Views
+public partial class AboutPage : ContentPage
 {
-    public partial class AboutPage : ContentPage
+    public AboutPage()
     {
-        public AboutPage()
-        {
-            InitializeComponent();
-        }
-
-        private async void LearnMore_Clicked(object sender, EventArgs e)
-        {
-            // Verificar si el BindingContext es del tipo Models.About
-            if (BindingContext is Models.About about)
-            {
-                // Navegar a la URL especificada en el navegador del sistema
-                if (!string.IsNullOrEmpty(about.MoreInfoUrl))
-                {
-                    await Launcher.Default.OpenAsync(about.MoreInfoUrl);
-                }
-                else
-                {
-                    await DisplayAlert("Error", "URL no especificada.", "OK");
-                }
-            }
-        }
+        InitializeComponent();
     }
 }
